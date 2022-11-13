23c3f09fb5ff464b986b8ad0145436314942aa52
Björn Höper <hoeper@ltsoft.de>
2022-11-13 09:55:40 +0100

    Implements event-driven submodel service provider
    Reaction to changes in the submodel possible by wrapping the actions on the submodel service provider in an event driven layer

---
+++ BaSyx.ServiceProvider.EventDriven/EventDrivenSubmodelServiceProviderFactory.cs
@@ -0,0 +1,39 @@
+﻿// /*******************************************************************************
+// * Copyright (c) 2023 LTSoft - Agentur für Leittechnik-Software GmbH
+// * Author: Björn Höper
+// *
+// * This program and the accompanying materials are made available under the
+// * terms of the Eclipse Public License 2.0 which is available at
+// * http://www.eclipse.org/legal/epl-2.0
+// *
+// * SPDX-License-Identifier: EPL-2.0
+// *******************************************************************************/
+
+using BaSyx.API.Components;
+using BaSyx.Models.Core.AssetAdministrationShell.Generics;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BaSyx.ServiceProvider.EventDriven;
+
+public class EventDrivenSubmodelServiceProviderFactory<TPersisting>: ISubmodelServiceProviderFactory 
+    where TPersisting: ISubmodelServiceProvider
+{
+    private readonly ILogger<EventDrivenSubmodelServiceProviderFactory<TPersisting>> _logger;
+    private readonly IServiceProvider _serviceProvider;
+
+
+    public EventDrivenSubmodelServiceProviderFactory(ILogger<EventDrivenSubmodelServiceProviderFactory<TPersisting>> logger, 
+        IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _serviceProvider = serviceProvider;
+    }
+
+    public ISubmodelServiceProvider CreateSubmodelServiceProvider(ISubmodel submodel)
+    {
+        var logger = _serviceProvider.GetRequiredService<ILogger<EventDrivenSubmodelServiceProvider<TPersisting>>>();
+        var persisting = _serviceProvider.GetRequiredService<TPersisting>();
+        return new EventDrivenSubmodelServiceProvider<TPersisting>(logger, persisting, submodel);
+    }
+}
