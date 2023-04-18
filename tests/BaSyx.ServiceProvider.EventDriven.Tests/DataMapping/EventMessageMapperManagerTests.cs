4e5aa815a2b29dbd29f1a88fdb769d1c350f159b
Björn Höper <hoeper@ltsoft.de>
2023-04-18 06:56:33 +0200

    Implements mapping events and bundling them in message mapper managers
    The mappers for the events are bundled in the mapper managers so that they can be used by the producers

---
+++ tests/BaSyx.ServiceProvider.EventDriven.Tests/DataMapping/EventMessageMapperManagerTests.cs
@@ -0,0 +1,93 @@
+﻿// /*******************************************************************************
+// * Copyright (c) 2023 LTSoft - Agentur für Leittechnik-Software GmbH
+// * Author: Björn Höper
+// *
+// * This program and the accompanying materials are made available under the
+// * terms of the Eclipse Public License 2.0 which is available at
+// * http://www.eclipse.org/legal/epl-2.0
+// *
+// * SPDX-License-Identifier: EPL-2.0
+// *******************************************************************************/
+
+using BaSyx.ServiceProvider.EventDriven.DataMapping;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace BaSyx.ServiceProvider.EventDriven.Tests.DataMapping;
+
+/// <summary>
+/// Test implemantation of <see cref="IEventMessageMapper{TSource,TMessage}"/>
+/// </summary>
+/// <typeparam name="TSource">Source type</typeparam>
+/// <typeparam name="TMessage">Message type to map to</typeparam>
+internal class TestEventMessageMapper<TSource, TMessage> : IEventMessageMapper<TSource, TMessage>
+{
+    public TMessage Map(TSource eventData)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class EventMessageMapperManagerTests
+{
+    [Fact]
+    public async Task GetMapper_WhenOneAvailable_GetsOne()
+    {
+        var loggerMock = new Mock<ILogger<EventMessageMapperManager>>();
+        var mapper1 = new TestEventMessageMapper<object, string>();
+
+        var mapperManager = new EventMessageMapperManager(loggerMock.Object, new[] { mapper1 });
+
+        var mappers = mapperManager.GetMapper<string>();
+        
+        Assert.Collection<IEventMessageMapper?>(mappers, m => Assert.Equal(m, mapper1));
+    }
+    
+    [Fact]
+    public async Task GetMapper_WhenOneOfTargetTypeAvailable_GetsOne()
+    {
+        var loggerMock = new Mock<ILogger<EventMessageMapperManager>>();
+        var mapper1 = new TestEventMessageMapper<object, string>();
+        var mapperWithDifferentTargetType = new TestEventMessageMapper<object, double>();
+
+        var mapperManager = new EventMessageMapperManager(loggerMock.Object, 
+            new IEventMessageMapper[] { mapper1, mapperWithDifferentTargetType });
+
+        var mappers = mapperManager.GetMapper<string>();
+        
+        Assert.Collection<IEventMessageMapper?>(mappers, m => Assert.Equal(m, mapper1));
+    }
+    
+    [Fact]
+    public async Task GetMapper_WhenTwoOfTargetTypeAvailable_GetsTwo()
+    {
+        var loggerMock = new Mock<ILogger<EventMessageMapperManager>>();
+        var mapper1 = new TestEventMessageMapper<object, string>();
+        var mapper2 = new TestEventMessageMapper<object, string>();
+        var mapperWithDifferentTargetType = new TestEventMessageMapper<object, double>();
+
+        var mapperManager = new EventMessageMapperManager(loggerMock.Object, 
+            new IEventMessageMapper[] { mapper1, mapper2, mapperWithDifferentTargetType });
+
+        var mappers = mapperManager.GetMapper<string>();
+        
+        Assert.Collection<IEventMessageMapper?>(mappers, 
+            m => Assert.Equal(m, mapper1), 
+            m => Assert.Equal(m , mapper2));
+    }
+    
+    [Fact]
+    public async Task GetMapper_WhenNoneOfTargetTypeAvailable_GetsEmptyEnumerable()
+    {
+        var loggerMock = new Mock<ILogger<EventMessageMapperManager>>();
+        
+        var mapperWithDifferentTargetType = new TestEventMessageMapper<object, double>();
+
+        var mapperManager = new EventMessageMapperManager(loggerMock.Object, 
+            new IEventMessageMapper[] { mapperWithDifferentTargetType });
+
+        var mappers = mapperManager.GetMapper<string>();
+        
+        Assert.Empty(mappers);
+    }
+}
